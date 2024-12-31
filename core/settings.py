115cb30bc4c87753b93ce315cbf2cae848299b7e
Usman <shahidusmaninfo@gmail.com>
2024-12-31 11:02:05 +0500

    add allows host

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ if not SECRET_KEY:
 # Render Deployment Code
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["34.209.210.215","127.0.0.1"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
